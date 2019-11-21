f2b24d95935c6c2efc24771005c287d9ea9976b0
Zack Wilcox <zwilcox96@gmail.com>
2019-11-20 22:29:48 -0800

    Removed unneeded css files

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,6 @@ import {AuthenticationService} from '../authentication.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
